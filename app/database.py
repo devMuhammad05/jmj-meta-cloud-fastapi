6c37bb236e376dcf3c7bf5a2a25a2293b0575d7d
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2026-03-27 21:30:32 +0100

    update

--- app/database.py
+++ app/database.py
@@ -1,8 +1,9 @@
+from urllib.parse import quote_plus
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{quote_plus(settings.database_username)}:{quote_plus(settings.database_password)}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
